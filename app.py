6855923e98d9a3401105a24f074b0fb667938a8f
rohit5683 <killerrohitrocks007@gmail.com>
2024-09-18 15:12:41 +0530

    CICD pipeline is implemented

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Azure Web Apps!"
+    return "Hello Azure Web Apps! CICD Pipline is Working fine"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
