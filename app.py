3895390aca904b0df1205599dacd16b9fe925512
rohit5683 <killerrohitrocks007@gmail.com>
2024-09-18 15:43:50 +0530

    new commit

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Azure Web Apps! CICD Pipline is working Successfully"
+    return "<html>
+    <h1>CICD Pipeline</h1>
+    </html>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
