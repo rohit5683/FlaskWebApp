81940ea1a9d2536631520cd7c4d63e8286119e5d
rohit5683 <killerrohitrocks007@gmail.com>
2024-09-18 15:16:02 +0530

    Pipeline is working fine

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Azure Web Apps! CICD Pipline is Working fine"
+    return "Hello Azure Web Apps! CICD Pipline is working Successfully"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
